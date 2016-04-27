c07cb96b62118aa17b0ada36893d81f3e21e71e1
chenlongzhen <760950023@qq.com>
2016-04-27 13:13:36 +0000

    2

--- src/tf-idf.py
+++ src/tf-idf.py
@@ -3,8 +3,12 @@
 #@author: BF-algo,longzhen.chen
 #sys.path.append('../')
 
+from logging.handlers import TimedRotatingFileHandler
+import logging
 from math import log
-import re,codecs,sys
+import re,codecs,sys,os
+import glob
+from multiprocessing import Pool
 import jieba
 import jieba.analyse
 from optparse import OptionParser
@@ -14,26 +18,9 @@ import numpy as np
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
 ROOT_PATH = sys.path[0]
 DATA_PATH = ROOT_PATH + "/../data/"
-
-#USAGE = "usage:    python extract_tags_idfpath.py [file name] -k [top k]"
-#jieba.load_userdict(DATA_PATH + 'dic_nospace_tag') 
-#parser = OptionParser(USAGE)
-#parser.add_option("-k", dest="topK")
-#opt, args = parser.parse_args()
-
-
-#if len(args) < 1:
-#    print(USAGE)
-#   sys.exit(1)
-
-#file_name = args[0]
-
-#if opt.topK is None:
-#    topK = 240
-#else:
-#    topK = int(opt.topK)
 
 index = defaultdict(list) 
 documents_num = 0 
@@ -62,28 +49,6 @@ def _unicodelist_to_str(word_list):
     
 def split_words(infile):
     """split words and return id [words] dataframe"""
-    #ids = []
-    #contents = []
-    # test
-    #with codecs.open(infile,'r') as infile:
-    #    for n,line in enumerate(infile):
-    #        #print line
-    #        if (n+1) % 3 == 0:
-    #            print "{} lines processed".format(n)      
-    #            exit -1
-    #        print _str_replace(line)
-    #        tokens = line.strip().split('\t')
-    #        print tokens
-    #        if not len(tokens) == 2:
-    #            continue
-    #        ids.append(tokens[0])
-    #        print tokens.encode
-    #        print "str {}".format(_str_replace(tokens[1]))
-    #        contents.append(str(_str_replace(tokens[1])))
-    #        print contents
-    #       
-            
-    #words_generaters = []
     def concat_str(data):
         """ concat same id's content"""
         #print content
@@ -91,10 +56,10 @@ def split_words(infile):
         return "".join(content_list)
     
     # read
-    print "--- read data ---"
+    logger.info("read data ")
     data = pd.read_table(infile,sep = "\t",names=['id','content'],encoding='utf-8',dtype={'id':np.str,'content':np.str})
     #print data
-    print "--- combine same id's content ---"
+    logger.info("combine same id's content ")
     data = pd.read_table(infile,sep = "\t",names=['id','content'],encoding='utf-8')
     combined_data = data.groupby('id').apply(concat_str)
     # sub
@@ -104,22 +69,17 @@ def split_words(infile):
     data_combined = pd.DataFrame({"id":ids_combined,"content":contents_combined}) 
     data_combined = data_combined[['id','content']]
     data = data_combined
-    #print data[:3]
-   
 
     # use jieba to split words 
-    
     word_generater =  data['content'].apply(jieba.cut)
     word_cut_list = word_generater.apply(list) # unicode type
-    #print word_cut_list
 
     # save
-    data['content'] = word_cut_list.apply(_unicodelist_to_str)
-    save_path = infile + "_splited"
-    data.to_csv(save_path,sep = "\t",encodeing='utf-8',header = False,index = False)
+    #data['content'] = word_cut_list.apply(_unicodelist_to_str)
+    #save_path = infile + "_splited"
+    #data.to_csv(save_path,sep = "\t",encodeing='utf-8',header = False,index = False)
     #print map(lambda x: x.encode("utf-8"),word_cut_list[0])[1]
 
-    # return id [words]
     data['content'] = word_cut_list
     return data 
 
@@ -144,7 +104,7 @@ def index_document(dataframe):
                 word_list.append(term) 
     return (index_document_dict,documents_num,word_list)    
     
-def idf(word_list,documents_num,index_document_dict):
+def idf(word_list,documents_num,index_document_dict,save_path):
     
     # idf
     idf_dict = defaultdict(float) 
@@ -154,11 +114,11 @@ def idf(word_list,documents_num,index_document_dict):
     # save
     idf_dict_dataframe = pd.DataFrame.from_dict(idf_dict,orient = 'index') 
     sorted_file = idf_dict_dataframe.sort_values(by=0,ascending=False)
-    sorted_file.to_csv(DATA_PATH + "idf.data",header = False)
+    sorted_file.to_csv(save_path ,header = False)
     #print sorted_file
     return dict(idf_dict)
         
-def tf_idf(dataframe,idf_dict,topk = 5):
+def tf_idf(dataframe,idf_dict,tf_idf_path,topk = 5):
    
     def _tf_idf(word_list):
         tf_idf_dict = defaultdict(float)
@@ -181,66 +141,72 @@ def tf_idf(dataframe,idf_dict,topk = 5):
         return key_value_str
      
     dataframe['content'] = dataframe['content'].apply(_tf_idf)
-    save_path = DATA_PATH + "tf_idf.data"
-    dataframe.to_csv(save_path,sep="\t",header = False,index = False)
-             
+    dataframe.to_csv(tf_idf_path,sep="\t",header = False,index = False)
     return dataframe
     
 
-def main():
+def process(file_path):
+    # parent parent dir 
+    path = os.path.dirname(file_path)
+    file_name = os.path.basename(file_path)
+    idf_path = path + "/../idf/"+file_name+"_idf.data"
+    tf_idf_path =path + "/../tf_idf/"+file_name+"_tf_idf.data"
+    word_dict_path = path + "/../word_dict/"+"id_word.data"
+
     # load dict
-    print "---load word dict ---"
-    jieba.load_userdict(DATA_PATH + "id_word.data") 
-    #   step 1. split content
-    print "---begin to read and split ---"
-    file_path = DATA_PATH + "id_post0_sample.txt" 
+    logger.info("load word dict ")
+    jieba.load_userdict(word_dict_path) 
+
+    # step 1. split content
+    logger.info("begin to read and split ")
     dataframe = split_words(file_path) 
-    #print dataframe
-    print "---construct index document ---"
+    #logger.info dataframe
+
+    logger.info("construct index document ")
     # step 2. get index document
     index_document_dict,documents_num,word_list = index_document(dataframe) 
     #print index_document_dict.items()[:10]
     #print documents_num
     #print word_list[:10]
+
     # step 3. idf
-    print "--- begin to count idf ---"
-    idf_dict =  idf(word_list,documents_num,index_document_dict)
+    logger.info(" begin to count idf ")
+    idf_dict =  idf(word_list,documents_num,index_document_dict,idf_path)
+
     # step 4. tf-idf
-    print "--- begin to count tfidf ---"
-    tf_idf_dict = tf_idf(dataframe,idf_dict) 
+    logger.info(" begin to count tfidf ")
+    tf_idf_dict = tf_idf(dataframe,idf_dict,tf_idf_path) 
     #print _str_replace("ä½ å¥½,123,ä½ å¥½,asdq")
     
-    
-    
-    
-    
-    
-    
-#    index = IrIndex()
-#    chunk = 100
-#    #content = open('user_corpus.txt', 'rb').read()
-#    #jieba.analyse.set_stop_words("./stop_words.txt")
-#    #jieba.analyse.set_idf_path("./weibo_tfidf_1029.txt")
-#    file_path = DATA_PATH + "id_post0_sample.txt" 
-#    id = []
-#    with open(file_path,"r") as rf:
-#        for n,line enumerate(rf):
-#            # print 
-#            if n % chunk == 0        
-#                print "{} line data have processed".format(n)
-#            tokens  = line.strip().split(" ")
-#            # format :id content, if not continue
-#            if not len(tokens) == 2:
-#                continue
-#            # get id and content
-#            id.append(tokens[0])
-#            content = tokens[1]
-#            # count tf and idf
-#            index.index_document(content)
-            
             
 if __name__ == "__main__":
-    main()
-            
+    ##  logging
+    ROOTPATH = sys.path[0] 
+    # set logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    
+    # create console handler and set level to debug
+    ch = TimedRotatingFileHandler(ROOTPATH + "/../log/main.log",when='D')
+    ch.setLevel(logging.DEBUG)
+    
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    
+    ## thread pool
+    file_res_path = "/home/chenlongzhen/IdeaProjects/td-idf/data/id_post"     
+    file_list = glob.glob(file_res_path + "/*")
+    logger.info("id_post files:")
+    logger.info("\n".join(file_list))
+    
+    pool = Pool(2)
+    processes = pool._pool
+    pool.map(process,file_list)
+    pool.close()
 
-
+    
+    
